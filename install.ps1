21a6e536ee45115510ed73fad95c52c8179c33e3
Martin Brandl <mjibrandl@gmail.com>
2021-11-03 08:59:53 +0100

    update choco script

--- install.ps1
+++ install.ps1
@@ -1,5 +1,15 @@
+# Install chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Find packages here: https://community.chocolatey.org/packages
 choco upgrade chocolatey
-choco install picpick.portable -y
+choco install picpick.portable -Y --force
+choco install powershell-core -y
+choco install terraform -y
+choco install microsoftazurestorageexplorer -y
+
+# retrieves information about packages that are outdated
+choco outdated
+
+# upgrade all packages
+choco upgrade all -y 
